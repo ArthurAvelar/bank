1eae40790ab381fb3804a1c8d221f3b1d7e9715c
ArthurAvelar <aavelae@sga.pucminas.br>
2020-08-03 12:36:50 -0300

    inclusao da Main

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,5 +1,9 @@
-public Class Aplicacao {
+public Class Aplicacao { 
+    public static void main (String args[]){
+        System.out.println("BEM vindo a central de atendimento do banco ");
+        
+            }
 
 
-    
+
 }
